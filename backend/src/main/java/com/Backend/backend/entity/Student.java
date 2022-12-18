9c8a44c3f9f1fc1ec834bc2e71ef0d34d802b759
amit sethi <amitsethi150@gmail.com>
2022-12-18 17:31:08 +0530

    mysql connected and student table created through entity class

---
+++ backend/src/main/java/com/Backend/backend/entity/Student.java
@@ -0,0 +1,21 @@
+package com.Backend.backend.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="student")
+public class Student {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	
+	private String name;
+	
+	private String about;
+
+}
